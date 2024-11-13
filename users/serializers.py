6779c674943ba69e91f4c1c2a88d2e4a06752b18
Yujin <ykim.code@gmail.com>
2024-11-13 13:21:12 -0500

    day17

--- users/serializers.py
+++ users/serializers.py
@@ -5,3 +5,20 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = '__all__' 
+
+        from rest_framework.serializers import ModelSerializer
+
+class PrivateUserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        exclude = (
+            "password",
+            "is_superuser",
+            "id",
+            "is_staff",
+            "is_active",
+            "first_name",
+            "last_name",
+            "groups",
+            "user_permissions",
+        )
--- users/urls.py
+++ users/urls.py
@@ -5,4 +5,8 @@ urlpatterns = [
     path('', views.Users.as_view()),
     path("<int:pk>/tweets", views.UserTweets.as_view()),
     path('<int:pk>/', views.UserDetails.as_view()),
+    path('me/', views.Me.as_view()),
+    path('password', views.ChangePassword.as_view()),
+    path('login/', views.LogIn.as_view()),
+    path('logout/', views.LogOut.as_view()),
 ]
